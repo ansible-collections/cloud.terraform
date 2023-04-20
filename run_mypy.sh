46e6070910924f9dc007face8e16ac0195d42e9d
abikouo <abikouo@redhat.com>
2023-04-20 13:55:44 +0200

    add changelog and linters github actions workflow

---
+++ run_mypy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# -*- coding: utf-8 -*-
+
+set -eux
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+mkdir -p ${SCRIPT_DIR}/.collection_root/ansible_collections/cloud
+ln -s ${SCRIPT_DIR} ${SCRIPT_DIR}/.collection_root/ansible_collections/cloud/terraform
+cd ${SCRIPT_DIR}/.collection_root/ansible_collections/cloud/terraform/
+export MYPYPATH="${SCRIPT_DIR}/.collection_root"
+mypy -p ansible_collections.cloud.terraform.plugins
+rm -rf ${SCRIPT_DIR}/.collection_root
