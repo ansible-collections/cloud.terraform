48c5a9ec640b013af9415cf97491206873274a47
abikouo <abikouo@redhat.com>
2023-04-20 15:46:07 +0200

    add sanity ignore

--- run_mypy.sh
+++ run_mypy.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
-# -*- coding: utf-8 -*-
-
 set -eux
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-mkdir -p ${SCRIPT_DIR}/.collection_root/ansible_collections/cloud
-ln -s ${SCRIPT_DIR} ${SCRIPT_DIR}/.collection_root/ansible_collections/cloud/terraform
-cd ${SCRIPT_DIR}/.collection_root/ansible_collections/cloud/terraform/
+mkdir -p "${SCRIPT_DIR}/.collection_root/ansible_collections/cloud"
+ln -s "${SCRIPT_DIR}" "${SCRIPT_DIR}/.collection_root/ansible_collections/cloud/terraform"
+cd "${SCRIPT_DIR}/.collection_root/ansible_collections/cloud/terraform/"
 export MYPYPATH="${SCRIPT_DIR}/.collection_root"
 mypy -p ansible_collections.cloud.terraform.plugins
-rm -rf ${SCRIPT_DIR}/.collection_root
+rm -rf "${SCRIPT_DIR}/.collection_root"
