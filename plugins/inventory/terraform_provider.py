2bf2b5e5f86f1935d41a9746d6241a50e9740fde
diademiemi <emilia@diademiemi.me>
2023-08-23 12:00:08 +0200

    Run black

--- plugins/inventory/terraform_provider.py
+++ plugins/inventory/terraform_provider.py
@@ -187,7 +187,9 @@ class InventoryModule(BaseInventoryPlugin):  # type: ignore  # mypy ignore
             for key, value in attributes.variables.items():
                 inventory.set_variable(attributes.name, key, value)
 
-    def create_inventory(self, inventory: Any, state_content: List[Optional[TerraformShow]], search_child_modules: bool) -> None:
+    def create_inventory(
+        self, inventory: Any, state_content: List[Optional[TerraformShow]], search_child_modules: bool
+    ) -> None:
         for state in state_content:
             if state is None:
                 continue
