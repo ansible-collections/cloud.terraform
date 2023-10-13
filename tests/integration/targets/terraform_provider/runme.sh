3d5f352d0427203c35db0ccbcddbecdb13b725a3
Bikouo Aubin <79859644+abikouo@users.noreply.github.com>
2023-10-13 07:54:32 +0200

    fix integration test target terraform_provider (#89)

--- tests/integration/targets/terraform_provider/runme.sh
+++ tests/integration/targets/terraform_provider/runme.sh
@@ -6,6 +6,7 @@ set -eux
     mkdir -p subdir2/
     cp test.yml subdir2/
     cp ansible_provider.tf subdir2/
+    cp -r modules subdir2
     cd subdir2/
     terraform init
     terraform apply -auto-approve -state mycustomstate.tfstate
@@ -16,6 +17,7 @@ ansible-playbook -i localhost, -i inventory2.yml test.yml
     mkdir -p subdir3/
     cp test.yml subdir3/
     cp ansible_provider.tf subdir3/
+    cp -r modules subdir3
     cd subdir3/
     terraform init
     terraform apply -auto-approve
@@ -26,6 +28,7 @@ ansible-playbook -i localhost, -i inventory3.yml test.yml
     mkdir -p subdir4/
     cp test.yml subdir4/
     cp ansible_provider.tf subdir4/
+    cp -r modules subdir4
     cd subdir4/
     terraform init
     terraform apply -auto-approve -state mystate.tfstate
