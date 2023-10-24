f23687064b1f5582fc6e08fdc14ab41b42e57190
Justin Cinkelj <justin.cinkelj@xlab.si>
2023-10-24 16:27:54 +0200

    Fix AWS SQS bug
    Fixes #86
    
    Signed-off-by: Justin Cinkelj <justin.cinkelj@xlab.si>

---
+++ tests/integration/targets/aws_sqs_queue/files/cloud.tf
@@ -0,0 +1,74 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "=4.38.0"
+    }
+  }
+}
+
+provider "aws" {
+}
+
+variable "cloud_terraform_integration_id" {
+  type = string
+}
+
+resource "aws_sqs_queue" "this" {
+  name = "${var.cloud_terraform_integration_id}-queue"
+  tags = {
+    Name                        = var.cloud_terraform_integration_id
+    cloud_terraform_integration = "true"
+  }
+}
+
+data "aws_iam_policy_document" "this" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type = "AWS"
+      identifiers = [
+        aws_iam_role.this.arn
+      ]
+    }
+
+    actions = [
+      "sqs:SendMessage"
+    ]
+
+    resources = [
+      aws_sqs_queue.this.arn
+    ]
+  }
+}
+
+resource "aws_sqs_queue_policy" "this" {
+  queue_url = aws_sqs_queue.this.id
+  policy    = data.aws_iam_policy_document.this.json
+}
+
+resource "aws_iam_role" "this" {
+  name = "${var.cloud_terraform_integration_id}-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Sid    = ""
+      Principal = {
+        Service = "lambda.amazonaws.com"
+      }
+    }]
+  })
+  tags = {
+    Name                        = var.cloud_terraform_integration_id
+    cloud_terraform_integration = "true"
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "this" {
+  role       = aws_iam_role.this.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
