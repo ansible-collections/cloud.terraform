df27b0a1675473a12a668d9879d5b903589716e8
Bikouo Aubin <79859644+abikouo@users.noreply.github.com>
2023-11-14 11:10:56 +0100

    add missing import

--- plugins/module_utils/models.py
+++ plugins/module_utils/models.py
@@ -1,5 +1,5 @@
 from typing import Any, Dict, List, Union, Optional
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 
 from ansible_collections.cloud.terraform.plugins.module_utils.types import AnyJsonType, TJsonObject
 
