7ae4ab818420a84492ebf4752af2f844d83fc588
abikouo <abikouo@redhat.com>
2024-01-25 14:00:54 +0100

    minor update on mypy script

--- run_mypy.sh
+++ run_mypy.sh
@@ -3,8 +3,9 @@ set -eux
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
+rm -rf "${SCRIPT_DIR}/.collection_root"
 mkdir -p "${SCRIPT_DIR}/.collection_root/ansible_collections/cloud"
-ln -s -n "${SCRIPT_DIR}" "${SCRIPT_DIR}/.collection_root/ansible_collections/cloud/terraform"
+ln -s "${SCRIPT_DIR}" "${SCRIPT_DIR}/.collection_root/ansible_collections/cloud/terraform"
 cd "${SCRIPT_DIR}/.collection_root/ansible_collections/cloud/terraform/"
 export MYPYPATH="${SCRIPT_DIR}/.collection_root"
 mypy -p ansible_collections.cloud.terraform.plugins
