ac4f4050fb566a72c2bad70a7335ebb41f27da71
aubin bikouo <abikouo@redhat.com>
2025-04-25 17:29:57 +0200

    Bump ansible-lint version to 25.1.3

--- tests/unit/plugins/modules/utils.py
+++ tests/unit/plugins/modules/utils.py
@@ -10,15 +10,6 @@ from unittest.mock import patch
 from ansible.module_utils import basic
 from ansible.module_utils.common.text.converters import to_bytes
 
-# def set_module_args(args):
-#     if "_ansible_remote_tmp" not in args:
-#         args["_ansible_remote_tmp"] = "/tmp"
-#     if "_ansible_keep_remote_files" not in args:
-#         args["_ansible_keep_remote_files"] = False
-
-#     args = json.dumps({"ANSIBLE_MODULE_ARGS": args})
-#     basic._ANSIBLE_ARGS = to_bytes(args)
-
 
 @contextlib.contextmanager
 def set_module_args(args):
