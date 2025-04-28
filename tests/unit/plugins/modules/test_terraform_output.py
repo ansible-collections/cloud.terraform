b04a48f18435df24f4dd7982841a88a5115c8fd9
Bikouo Aubin <79859644+abikouo@users.noreply.github.com>
2025-04-28 14:55:46 +0200

    Fixing unit tests with devel version (#176)
    * Fixing unit tests with devel version
    * Bump ansible-lint version to 25.1.3
    
    (cherry picked from commit ed79d761b7cc80161a6b32d744d374cb46e357dd)

--- tests/unit/plugins/modules/test_terraform_output.py
+++ tests/unit/plugins/modules/test_terraform_output.py
@@ -20,7 +20,12 @@ from ansible_collections.cloud.terraform.tests.unit.plugins.modules.utils import
 class TestTerraformOutputMain:
     def test_return_selected_output(self, mocker):
         mocker.patch.multiple(basic.AnsibleModule, exit_json=exit_json, fail_json=fail_json)
-        set_module_args(
+        mocker.patch("ansible_collections.cloud.terraform.plugins.modules.terraform_output.validate_bin_path")
+        mocker.patch(
+            "ansible_collections.cloud.terraform.plugins.modules.terraform_output.get_outputs"
+        ).return_value = "my_output_value"
+
+        with set_module_args(
             {
                 "project_path": "path/to/project",
                 "name": "my_output",
@@ -28,81 +33,60 @@ class TestTerraformOutputMain:
                 "binary_path": "terraform/binary/path",
                 "state_file": "mystate.tfstate",
             }
-        )
-        mocker.patch("ansible_collections.cloud.terraform.plugins.modules.terraform_output.validate_bin_path")
-        mocker.patch(
-            "ansible_collections.cloud.terraform.plugins.modules.terraform_output.get_outputs"
-        ).return_value = "my_output_value"
+        ):
+            with pytest.raises(AnsibleExitJson) as exc:
+                terraform_output.main()
 
-        with pytest.raises(AnsibleExitJson) as exc:
-            terraform_output.main()
-
-        assert exc.value.args[0]["value"] == "my_output_value"
+            assert exc.value.args[0]["value"] == "my_output_value"
 
     def test_return_all_outputs(self, mocker):
         mocker.patch.multiple(basic.AnsibleModule, exit_json=exit_json, fail_json=fail_json)
-        set_module_args(
+        mocker.patch("ansible_collections.cloud.terraform.plugins.modules.terraform_output.validate_bin_path")
+        mocker.patch(
+            "ansible_collections.cloud.terraform.plugins.modules.terraform_output.get_outputs"
+        ).return_value = {"my_output": {"sensitive": False, "type": "string", "value": "file generated"}}
+
+        with set_module_args(
             {
                 "project_path": "path/to/project",
                 "format": "json",
                 "binary_path": "terraform/binary/path",
                 "state_file": "mystate.tfstate",
             }
-        )
-        mocker.patch("ansible_collections.cloud.terraform.plugins.modules.terraform_output.validate_bin_path")
-        mocker.patch(
-            "ansible_collections.cloud.terraform.plugins.modules.terraform_output.get_outputs"
-        ).return_value = {"my_output": {"sensitive": False, "type": "string", "value": "file generated"}}
+        ):
+            with pytest.raises(AnsibleExitJson) as exc:
+                terraform_output.main()
 
-        with pytest.raises(AnsibleExitJson) as exc:
-            terraform_output.main()
-
-        assert exc.value.args[0]["outputs"] == {
-            "my_output": {"sensitive": False, "type": "string", "value": "file generated"}
-        }
+            assert exc.value.args[0]["outputs"] == {
+                "my_output": {"sensitive": False, "type": "string", "value": "file generated"}
+            }
 
     def test_required_if(self, mocker):
         mocker.patch.multiple(basic.AnsibleModule, exit_json=exit_json, fail_json=fail_json)
-        set_module_args(
+        mocker.patch("ansible_collections.cloud.terraform.plugins.modules.terraform_output.validate_bin_path")
+        mocker.patch("ansible_collections.cloud.terraform.plugins.modules.terraform_output.get_outputs")
+
+        with set_module_args(
             {
                 "project_path": "path/to/project",
                 "format": "raw",
                 "binary_path": "terraform/binary/path",
                 "state_file": "mystate.tfstate",
             }
-        )
-        mocker.patch("ansible_collections.cloud.terraform.plugins.modules.terraform_output.validate_bin_path")
-        mocker.patch("ansible_collections.cloud.terraform.plugins.modules.terraform_output.get_outputs")
+        ):
+            with pytest.raises(AnsibleFailJson) as exc:
+                terraform_output.main()
 
-        with pytest.raises(AnsibleFailJson) as exc:
-            terraform_output.main()
-
-        assert exc.value.args[0]["msg"] == "format is raw but all of the following are missing: name"
+            assert exc.value.args[0]["msg"] == "format is raw but all of the following are missing: name"
 
     def test_except_terraform_warning(self, mocker):
         mocker.patch.multiple(basic.AnsibleModule, exit_json=exit_json, fail_json=fail_json)
-        set_module_args(
-            {
-                "project_path": "path/to/project",
-                "name": "my_output",
-                "format": "json",
-                "binary_path": "terraform/binary/path",
-                "state_file": "mystate.tfstate",
-            }
-        )
         mocker.patch("ansible_collections.cloud.terraform.plugins.modules.terraform_output.validate_bin_path")
         mocker.patch(
             "ansible_collections.cloud.terraform.plugins.modules.terraform_output.get_outputs"
         ).side_effect = TerraformWarning("Could not get Terraform outputs.")
 
-        with pytest.raises(AnsibleExitJson) as exc:
-            terraform_output.main()
-
-        assert exc.value.args[0]["value"] is None
-
-    def test_except_terraform_error(self, mocker):
-        mocker.patch.multiple(basic.AnsibleModule, exit_json=exit_json, fail_json=fail_json)
-        set_module_args(
+        with set_module_args(
             {
                 "project_path": "path/to/project",
                 "name": "my_output",
@@ -110,13 +94,28 @@ class TestTerraformOutputMain:
                 "binary_path": "terraform/binary/path",
                 "state_file": "mystate.tfstate",
             }
-        )
+        ):
+            with pytest.raises(AnsibleExitJson) as exc:
+                terraform_output.main()
+
+            assert exc.value.args[0]["value"] is None
+
+    def test_except_terraform_error(self, mocker):
+        mocker.patch.multiple(basic.AnsibleModule, exit_json=exit_json, fail_json=fail_json)
         mocker.patch("ansible_collections.cloud.terraform.plugins.modules.terraform_output.validate_bin_path")
         mocker.patch(
             "ansible_collections.cloud.terraform.plugins.modules.terraform_output.get_outputs"
         ).side_effect = TerraformError("Failure when getting Terraform outputs.")
+        with set_module_args(
+            {
+                "project_path": "path/to/project",
+                "name": "my_output",
+                "format": "json",
+                "binary_path": "terraform/binary/path",
+                "state_file": "mystate.tfstate",
+            }
+        ):
+            with pytest.raises(AnsibleFailJson) as exc:
+                terraform_output.main()
 
-        with pytest.raises(AnsibleFailJson) as exc:
-            terraform_output.main()
-
-        assert exc.value.args[0]["msg"] == "Failure when getting Terraform outputs."
+            assert exc.value.args[0]["msg"] == "Failure when getting Terraform outputs."
